b3efb00c6542c37eb2c4710ee1774b81558557b6
Ryan <ryanpburnett@yahoo.com>
2021-12-15 16:44:11 -0600

    new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses.component';
+import { TestComponent } from './test.conponent';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    CoursesComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/test.conponent.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'test',
+    template: `<p>text1</p>
+    <p>text2</p>
+    <p>text3</p>
+    <p>text4</p>`
+})
+export class TestComponent {
+
+}
