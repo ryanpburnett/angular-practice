0945572033e3e09d1866b034d089b8583aeff3f2
Ryan <ryanpburnett@yahoo.com>
2021-12-16 15:21:38 -0600

    messing with css

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core'
 @Component({
     selector: 'courses',
     template: `
+        <h1>I ran a function just for trying it out: {{ heresAFunction(1) }}
         <h2>{{ "here is the title: " + getTitle() }}</h2>
         <ul>
             <li *ngFor="let course of courses">
@@ -15,6 +16,9 @@ export class CoursesComponent {
     title = "List of courses"
     getTitle() {
         return this.title;
+    }
+    heresAFunction(x: number) {
+        return x*412;
     }
     courses = ["course1", "course2", "course3"]
 }
