7b35b6e9d8cbcfe28184103dda928929ac6bf629
Ryan <ryanpburnett@yahoo.com>
2021-12-16 13:09:00 -0600

    added html template for test component

--- src/app/silly.component.ts
+++ src/app/silly.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'silly',
-    template: `<img src="dog.jpg">`
+    template: '<img src="./asssets/dog.jpg" alt="">'
 })
 export class SillyComponent {
 
--- src/app/test.component.ts
+++ src/app/test.component.ts
@@ -2,10 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'test',
-    template: `<p>text1</p>
-    <p>text2</p>
-    <p>text3</p>
-    <p>text4</p>`
+    templateUrl: './test.component.html'
 })
 export class TestComponent {
 
