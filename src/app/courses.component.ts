3d9324907a658759c5eb61b96c4ce66eb3fdbbb6
Ryan <ryanpburnett@yahoo.com>
2021-12-16 14:38:39 -0600

    template work, interpolation

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -2,8 +2,19 @@ import { Component } from '@angular/core'
 
 @Component({
     selector: 'courses',
-    template: '<h2>Courses</h2>'
+    template: `
+        <h2>{{ "here is the title: " + getTitle() }}</h2>
+        <ul>
+            <li *ngFor="let course of courses">
+                {{ course }}
+            </li>
+        </ul>
+    `
 })
 export class CoursesComponent {
-
+    title = "List of courses"
+    getTitle() {
+        return this.title;
+    }
+    courses = ["course1", "course2", "course3"]
 }
