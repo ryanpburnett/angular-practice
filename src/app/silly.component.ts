92e22290378e211d42cb3cff1a43f68cc19b18dd
Ryan <ryanpburnett@yahoo.com>
2021-12-28 15:44:12 -0600

    image tag working

--- src/app/silly.component.ts
+++ src/app/silly.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'silly',
-    template: '<img src="./asssets/dog.jpg" alt="">'
+    template: '<p>image:</p><img src="./assets/dog.jpg" alt="">'
 })
 export class SillyComponent {
 
