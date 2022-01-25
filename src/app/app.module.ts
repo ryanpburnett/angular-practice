de8a93d3f3355c88a57fe4b5f37a467d12883adb
Ryan <ryanpburnett@yahoo.com>
2022-01-25 14:49:35 -0600

    fixed silly component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses.component';
-import { SillyComponent } from './silly.component';
+import { SillyComponent } from './silly/silly.component';
 import { TestComponent } from './test.component';
 import { PancakesComponent } from './pancakes/pancakes.component';
 import { NewGeneratedComponentComponent } from './new-generated-component/new-generated-component.component';
